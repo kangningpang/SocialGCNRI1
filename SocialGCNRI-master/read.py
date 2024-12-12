557b7b8e8c9a0ee33fed109d950c6ab258d760fa
kangningpang <1292843384@qq.com>
2024-12-12 10:40:12 +0800

    read.py

--- SocialGCNRI-master/read.py
+++ SocialGCNRI-master/read.py
@@ -1,5 +1,5 @@
 import torch
 
-f = open('D:\研究生\研二\\2024.4-7\实验\SocialLGN\SocialLGN-master\data.pkl','rb')
+f = open('filename','rb')
 data = torch.load(f,map_location='cpu')#可使用cpu或gpu
 print(data)
